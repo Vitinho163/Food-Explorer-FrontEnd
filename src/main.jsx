2eca901b5484754eb350f7a32495ab72902b1178
João Victor <victor.jvsb18@gmail.com>
2024-02-25 16:38:06 -0300

    Setup: React Router, Pages, Icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { Home } from './pages/Home'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
