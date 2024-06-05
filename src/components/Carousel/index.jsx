4f8bad9e1b64dfe84cdacd64472f10041d189c96
João Victor <victor.jvsb18@gmail.com>
2024-06-05 20:08:54 -0300

    Add navigation icons for carousel scrolling

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Product } from '../Product'
 import UseEmblaCarousel from 'embla-carousel-react'
-import { Embla, EmblaContainer, EmblaViewport } from './styles'
+import { CarouselButton, Embla, EmblaContainer, EmblaViewport } from './styles'
+import IconRight from '../../assets/IconRight.svg'
+import IconLeft from '../../assets/IconLeft.svg'
 
 export function Carousel({
   options,
@@ -14,6 +16,9 @@ export function Carousel({
 
   return (
     <Embla>
+      <CarouselButton className="left" onClick={() => emblaApi.scrollPrev()}>
+        <img src={IconLeft} alt="Seta para direita" />
+      </CarouselButton>
       <EmblaViewport ref={emblaRef}>
         <EmblaContainer>
           {products.map((product) => (
@@ -27,6 +32,9 @@ export function Carousel({
           ))}
         </EmblaContainer>
       </EmblaViewport>
+      <CarouselButton className="right" onClick={() => emblaApi.scrollNext()}>
+        <img src={IconRight} alt="Seta para esquerda" />
+      </CarouselButton>
     </Embla>
   )
 }
--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const Embla = styled.div`
+  position: relative;
   --slide-spacing: 1rem;
   --slide-size: auto;
   --slide-height: 19rem;
@@ -15,4 +16,25 @@ export const EmblaContainer = styled.div`
   display: flex;
   gap: 2.8rem;
   touch-action: pan-y;
+`
+
+export const CarouselButton = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.dark_100};
+  cursor: pointer;
+  z-index: 1;
+
+  &.left {
+    left: -1rem;
+  }
+
+  &.right {
+    right: -1rem;
+  }
 `
